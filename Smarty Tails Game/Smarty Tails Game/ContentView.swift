da0a81e9dfcc2ef3286e0082e59be711e09cab7a
Maxim Fedoseenko <158294939+mfcodess@users.noreply.github.com>
2026-07-07 21:00:15 +0300

    Initial project upload

---
+++ Smarty Tails Game/Smarty Tails Game/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Smarty Tails Game
+//
+//  Created by Максим  on 07.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
